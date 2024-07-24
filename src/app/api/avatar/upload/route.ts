48cc224169dfca776017754d0157de3db424bb65
ashrafabusunaina1986 <ashrafabusunaina1986@gmail.com>
2024-07-25 01:15:38 +0300

    first commit

--- src/app/api/avatar/upload/route.ts
+++ src/app/api/avatar/upload/route.ts
@@ -1,17 +1,14 @@
 import { NextResponse } from "next/server";
 import { put } from "@vercel/blob";
-export async function POST(request: Request): Promise<NextResponse> {
-  const { searchParams } = new URL(request.url);
-  const filename = searchParams.get("filename") ;
 
-  // ⚠️ The below code is for App Router Route Handlers only
+export async function POST(req: Request): Promise<NextResponse> {
+  const { searchParams } = new URL(req.url);
+  const filename = searchParams.get("filename") as string;
+  const blob = await put(filename, req.body as ReadableStream, {
+    access: "public",
+  });
 
-  const blob = await put(
-    `subjects/${filename}`,
-    request.body as ReadableStream,
-    {
-      access: "public",
-    }
-  );
+  // db();
+  // await Images.create({ filename: blob.pathname, url: blob.url });
   return NextResponse.json(blob);
 }
