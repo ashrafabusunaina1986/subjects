308c3bb1ca507986baa899bd4f7d106542177173
ashrafabusunaina1986 <ashrafabusunaina1986@gmail.com>
2024-07-25 15:46:56 +0300

    first commit

--- src/components/add-subject/index.tsx
+++ src/components/add-subject/index.tsx
@@ -1,9 +1,17 @@
 "use client";
 import React, { useContext } from "react";
-import { Dialog, DialogContent, DialogHeader, DialogTitle } from "../ui/dialog";
+import {
+  Dialog,
+  DialogClose,
+  DialogContent,
+  DialogHeader,
+  DialogTitle,
+} from "../ui/dialog";
 import { BtnContext } from "@/context";
 import Cf from "../cf";
 import { initialFcs, v } from "@/app/utils";
+import { RiChatDeleteFill } from "react-icons/ri";
+
 
 function AddSubject({
   ref,
@@ -22,19 +30,28 @@ function AddSubject({
   return (
     <Dialog
       open={val.stateDialog}
-      onOpenChange={() => {
-        setVal({ stateDialog: false });
-        setDataForm(initialFcs);
-        setSelectImg("");
-      }}
+      // onOpenChange={() => {
+      //   setVal({ stateDialog: false });
+      //   setDataForm(initialFcs);
+      //   setSelectImg("");
+      // }}
     >
       <DialogContent className=" overflow-y-auto h-[500px]">
+        
+          <RiChatDeleteFill className=" absolute right-3 top-3 z-50  w-[30px] h-[30px] rounded-md "
+            onClick={() => {
+              setVal({ stateDialog: false });
+              setDataForm(initialFcs);
+              setSelectImg("");
+            }}
+          />
+
         <DialogHeader className="">
           <DialogTitle>Add Subject</DialogTitle>
         </DialogHeader>
         <Cf
-        ref={ref}
-        ons={ons}
+          ref={ref}
+          ons={ons}
           loading={loading}
           handleAction={handleAddSubjectAction}
           data={dataForm}
